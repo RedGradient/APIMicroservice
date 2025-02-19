4cf7ea1e2b3f2d02cd67a0453b8672e19ac27444
redgradient <neutral1shade@yahoo.com>
2025-02-19 23:36:05 +0400

    obtain host of authentication server from environment variable

--- src/config.py
+++ src/config.py
@@ -1,2 +1,5 @@
-PUBLIC_KEYS_URL: str = "http://127.0.0.1:8000/public-keys"
+import os
+
+AUTH_HOST = os.getenv("AUTH_HOST", "localhost")
+PUBLIC_KEYS_URL: str = f"http://{AUTH_HOST}:8000/public-keys"
 ALGORITHM: str = "RS256"
